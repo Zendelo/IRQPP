40a1fa6b3a9d8dcd5b0e3af5791870e349f07c32
Zendelo <olegzendel@campus.technion.ac.il>
2018-05-12 09:52:53 +0300

    fix small bugs

--- eval.py
+++ eval.py
@@ -17,7 +17,7 @@ parser.add_argument('--predictor', metavar='predictor_file_path',
                     default='SetupFiles-indri-5.6/clarity.m-1/Clarity-Fiana', help='path to predictor executable file')
 parser.add_argument('--parameters', metavar='parameters_file_path', default='clarity/clarityParam.xml',
                     help='path to predictor parameters file')
-parser.add_argument('--testing', metavar='running_parameter', default='-documents=', choices=['-documents'],
+parser.add_argument('--testing', metavar='running_parameter', default='-documents=', choices=['documents', 'fbDocs'],
                     help='The parameter to optimize')
 parser.add_argument('-q', '--queries', metavar='queries.xml', default='data/ROBUST/queries.xml',
                     help='path to queries xml file')
@@ -38,7 +38,7 @@ def pre_testing(predictor_exe, parameters_xml, test_param, queries):
     for i in PARAMS:
         print('\n ******** Running for: {} documents ******** \n'.format(i))
         output = 'tmp-testing/clarity-{}/predictions-{}'.format(pred, i)
-        run('{} {} {}{} {} > {}'.format(predictor_exe, parameters_xml, test_param, i,
+        run('{} {} -{}={} {} > {}'.format(predictor_exe, parameters_xml, test_param, i,
                                         queries, output), shell=True)
 
 
