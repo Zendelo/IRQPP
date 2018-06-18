f70ffa4521ccecfb1b86898f61146d0dea3171af
Zendelo <olegzendel@campus.technion.ac.il>
2018-06-18 20:52:58 +0300

    add script to create aggregates

--- createAggregations.sh
+++ createAggregations.sh
@@ -1,13 +1,14 @@
 #! /usr/bin/env bash
+#This script is running the python code that calculates the aggregations
 
-for i in {5,10,25,50,100,250,500,1000}
+for p in {'clarity','wig','nqc'}
 do
-	for k in {5,10,25,50,100}
+	for i in {5,10,25,50,100,250,500,1000}
+do
+	for j in {'min','max','sum','avg'}
 	do
-		for j in {'min','max','std','avg'}
-			do
 		printf "\n ******** Running for: $i docs $j function ******** \n"
-		python3.6 ~/repos/IRQPP/aggregateUQV.py -p ./predictions/predictions-$i+$k -f $j >	./aggr/$j/predictions-$i+$k
+		python3.6 ~/repos/IRQPP/aggregateUQV.py -p ~/predictionsUQV/$p/predictions/predictions-$i -f $j >	~/predictionsUQV/$p/aggr/$j/predictions-$i
 	done
 done
 done
