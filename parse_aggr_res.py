d552c80e499f147cb94b0986290718c95cb0607d
Zendelo <olegzendel@campus.technion.ac.il>
2018-07-12 19:41:49 +0300

    cleanup code and add documentation

--- parse_aggr_res.py
+++ parse_aggr_res.py
@@ -1,9 +1,9 @@
-import pandas as pd
+import argparse
 import csv
 from collections import defaultdict
-import argparse
 from functools import reduce
-import numpy as np
+
+import pandas as pd
 
 parser = argparse.ArgumentParser(description='UQV aggregation script',
                                  usage='Create new UQV scores',
@@ -14,6 +14,8 @@ parser.add_argument('--ap', default=False, action='store_true')
 
 
 def read_file(file):
+    """Reads the results file into a dictionary"""
+    predictor, ap, pred, mean, var, std = None, None, None, None, None, None
     temp_dict = defaultdict(list)
     with open(file, 'r') as f:
         reader = csv.reader(f)
@@ -22,7 +24,6 @@ def read_file(file):
                 continue
             else:
                 row = row[0].split()
-                # print(row)
                 if row[0].lower() == 'predictor':
                     predictor = row[1]
                     continue
@@ -38,44 +39,14 @@ def read_file(file):
                     continue
                 elif row[0].lower().startswith('stand'):
                     std = row[3]
-                # temp_dict[predictor].append([ap, pred, mean, var, std])
-                temp_dict[predictor].append({'ap': ap, 'predictor': pred, 'mean': mean, 'var': var, 'std': std})
-    f.close()
-    return temp_dict
-
-
-def read_big_file(file):
-    temp_dict = defaultdict(list)
-    with open(file, 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if len(row) == 0:
-                continue
-            else:
-                row = row[0].split()
-                # print(row)
-                if row[0].lower() == 'predictor':
-                    predictor = row[1]
-                    continue
-                elif row[0].lower().startswith('ap-'):
-                    ap = row[0].split('-')[1]
-                    pred = row[1].split('-')[1]
-                    continue
-                elif row[0].lower().startswith('mean'):
-                    mean = row[2]
-                    continue
-                elif row[0].lower().startswith('var'):
-                    var = row[2]
-                    continue
-                elif row[0].lower().startswith('stand'):
-                    std = row[3]
-                # temp_dict[predictor].append([ap, pred, mean, var, std])
                 temp_dict[predictor].append({'ap': ap, 'predictor': pred, 'mean': mean, 'var': var, 'std': std})
     f.close()
     return temp_dict
 
 
 def read_ap_file(file):
+    """Reads the AP results file into a dictionary"""
+    ap, pred, mean = None, None, None
     temp = list()
     with open(file, 'r') as f:
         reader = csv.reader(f)
@@ -96,7 +67,8 @@ def read_ap_file(file):
     return temp
 
 
-def print_latex(res_dict, p=None):
+def print_latex(res_dict):
+    """Returns a dict of small tables latex strings"""
     df = pd.DataFrame.from_records(res_dict, columns=['ap', 'predictor', 'mean'])
     df = df.set_index(['ap'])
     df = df.sort_index()
@@ -109,7 +81,6 @@ def print_latex(res_dict, p=None):
     x = reduce(lambda left, right: pd.merge(left, right, on='predictor'), [avg_df, max_df, med_df, min_df, std_df])
     x = x.set_index('predictor')
     x.columns = ['avg', 'max', 'med', 'min', 'std']
-    # print(x.to_latex())
     return x.to_dict(orient='dict')
 
 
@@ -126,17 +97,13 @@ def bold_max_col(res_dict):
                 max_val = _max_val
                 max_pred = {'aggregate': max_agg, 'predictor': pred, 'corr': max_val}
         res_dict[max_pred['predictor']][agg][max_pred['aggregate']] = '\\textbf{{{}}}'.format(max_pred['corr'])
+        max_cols[agg] = max_pred
+    return max_cols
 
 
 def print_big_latex(res_dict):
-    # test = list()
+    """Merge and prints the large table latex string"""
     test = defaultdict()
-    avg_dict = defaultdict(list)
-    max_dict = defaultdict(list)
-    med_dict = defaultdict(list)
-    min_dict = defaultdict(list)
-    std_dict = defaultdict(list)
-    # pred_list = ['clarity', 'wig', 'nqc', 'qf']
     agg_list = aggregates_list
     for pred in agg_list:
         for ap in agg_list:
@@ -144,7 +111,6 @@ def print_big_latex(res_dict):
                               'wig': '${}$'.format(res_dict['wig'][ap][pred]),
                               'nqc': '${}$'.format(res_dict['nqc'][ap][pred]),
                               'qf': '${}$'.format(res_dict['qf'][ap][pred])}
-    # print(pd.DataFrame.from_dict(test['avg', 'avg'], orient='index'))
     col = 0
     print('\\begin{tabular}{lccccc}')
     print('\\toprule')
@@ -154,9 +120,7 @@ def print_big_latex(res_dict):
 
     for pred in agg_list:
         for ap in agg_list:
-            # print('pred {}'.format(pred), 'ap {}'.format(ap))
             x = pd.DataFrame.from_dict(test[ap, pred], orient='index')
-
             if col % 5 == 0:
                 print('{}   &'.format(pred))
                 latex = x.to_latex(header=False, multirow=True, index=True, escape=False)
@@ -166,14 +130,12 @@ def print_big_latex(res_dict):
             latex = latex.replace('\\bottomrule', '')
             print(latex)
             col += 1
-
             if col % 5 == 0:
                 print('\\\\')
                 if pred != 'std':
                     print('\\hline')
             else:
                 print('&')
-
     print('\\bottomrule')
     print('\\end{tabular}')
 
@@ -189,7 +151,7 @@ def main(args: parser):
         _dict = defaultdict()
         res_dic = read_file(results_file)
         for p in res_dic:
-            _dict[p] = print_latex(res_dic[p], p)
+            _dict[p] = print_latex(res_dic[p])
         bold_max_col(_dict)
         print_big_latex(_dict)
 
