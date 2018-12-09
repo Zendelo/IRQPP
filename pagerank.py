746995af0f34506847e09f208d685a99c8ba7d49
Zendelo <olegzendel@campus.technion.ac.il>
2018-12-09 14:00:09 +0200

    first working pagerank code

--- pagerank.py
+++ pagerank.py
@@ -1,61 +1,149 @@
 import argparse
 import multiprocessing as mp
-import os
-from collections import defaultdict
-from itertools import combinations_with_replacement
 from functools import partial
+import pickle
 
 import numpy as np
 import pandas as pd
 
 import dataparser as dp
-from RBO import rbo_dict
 from topic_graph_features import features_loader
 from Timer.timer import Timer
+from crossval import CrossValidation
 
-parser = argparse.ArgumentParser(description='Features for PageRank UQV query variations Generator',
-                                 usage='python3.7 features.py -q queries.txt -c CORPUS',
+parser = argparse.ArgumentParser(description='PageRank UQV Generator',
+                                 usage='python3.7 pagerank.py -c CORPUS',
                                  epilog='Unless --generate is given, will try loading the file')
 
 parser.add_argument('-c', '--corpus', default='ROBUST', type=str, help='corpus (index) to work with',
                     choices=['ROBUST', 'ClueWeb12B'])
+parser.add_argument('-p', '--predictor', default=None, type=str, help='Choose the predictor to use',
+                    choices=['clarity', 'wig', 'nqc', 'qf', 'all'])
 
 # parser.add_argument('-g', '--group', help='group of queries to predict',
 #                     choices=['top', 'low', 'medh', 'medl', 'title'])
 # parser.add_argument('--quantile', help='quantile of query variants to use for prediction', default=None,
 #                     choices=['all', 'low', 'med', 'top'])
-parser.add_argument('-l', '--load', default=None, type=str, help='features file to load')
-parser.add_argument('--generate', help="generate new features file", action="store_true")
-parser.add_argument('--predict', help="generate new predictions", action="store_true")
+# parser.add_argument('-l', '--load', default=None, type=str, help='features file to load')
+# parser.add_argument('--generate', help="generate new features file", action="store_true")
+# parser.add_argument('--predict', help="generate new predictions", action="store_true")
+
+LAMBDA = np.linspace(start=0, stop=1, num=11)
+
+
+class PageRank:
+    def __init__(self, corpus, predictor, load=False):
+        self.corpus = corpus
+        self.__set_paths(corpus, predictor)
+        self.similarity_features_df = self.__initialize_features_df()
+        self.similarity_measures = self.similarity_features_df.columns.tolist()
+        self.var_cv = CrossValidation(file_to_load=self.folds, predictions_dir=self.vars_results_dir)
+        self.prediction_scores = self.var_cv.full_set.columns.tolist()
+        self.full_raw_weights_df = self.__initialize_full_scores_df()
+        self.dict_all_options = self._set_weights()
+        try:
+            file_to_load = dp.ensure_file(f'~/pagerank_{corpus}_{predictor}_dict_all_options_stochastic.pkl')
+            with open(file_to_load, 'rb') as handle:
+                self.dict_all_options_stochastic = pickle.load(handle)
+        except AssertionError:
+            self.dict_all_options_stochastic = self._normalize_rows()
+            _dir = dp.ensure_dir('~/')
+            with open(f'{_dir}/pagerank_{corpus}_{predictor}_dict_all_options_stochastic.pkl', 'wb') as handle:
+                pickle.dump(self.dict_all_options_stochastic, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    @classmethod
+    def __set_paths(cls, corpus, predictor):
+        """This method sets the default paths of the files and the working directories, it assumes the standard naming
+         convention of the project"""
+        cls.predictor = predictor
+
+        _base_dir = f'~/QppUqvProj/Results/{corpus}/uqvPredictions/'
+        cls.vars_results_dir = dp.ensure_dir(f'{_base_dir}/raw/{cls.predictor}/predictions/')
+
+        cls.output_dir = dp.ensure_dir(f'{_base_dir}/referenceLists/pageRank/')
+
+        _test_dir = f'~/QppUqvProj/Results/{corpus}/test'
+        cls.folds = dp.ensure_file(f'{_test_dir}/2_folds_30_repetitions.json')
+
+        cls.ap_file = dp.ensure_file(f'{_test_dir}/pageRank/QLmap1000')
+
+        cls.features = dp.ensure_file(f'{_test_dir}/pageRank/{corpus}_raw_PageRank_Features.pkl')
+
+    def __initialize_features_df(self):
+        """This method loads the features df from a pickle file"""
+        _features_df = features_loader(self.corpus, self.features)
+        return _features_df
+
+    def __initialize_full_scores_df(self):
+        """This method filters from query variations df only the ones relevant for prediction"""
+        _var_scores_df = pd.merge(self.similarity_features_df, self.var_cv.full_set, left_on='dest', how='left',
+                                  right_index=True)
+        return _var_scores_df
+
+    def _set_weights(self):
+        # TODO: Implement a special case for lambda=1
+        """This method implements the linear interpolation of the weight function"""
+        _dict = {}
+        for lambda_param in LAMBDA:
+            for similarity in self.similarity_measures:
+                _pred_scores_df = self.full_raw_weights_df[self.prediction_scores] * (1 - lambda_param)
+                _sim_df = self.full_raw_weights_df[similarity] * lambda_param
+                _dict[f'simfunc-{similarity}+lambda-{lambda_param}'] = _pred_scores_df.add(_sim_df, axis=0)
+        return _dict
+
+    def _normalize_rows(self):
+        """This method will normalize the weights of each row in the matrix W to be equal to 1 in order to make it
+        a legal right stochastic matrix. The rows of matrix W are all the outgoing edges from src node i"""
+        _dict = {}
+        for params, df in self.dict_all_options.items():
+            _list = []
+            for src_qid, _df in df.groupby(level='src'):
+                _norm_df = _df / _df.sum()
+                _list.append(_norm_df)
+            _dict[params] = pd.concat(_list)
+        return _dict
+
+    def calc_pagerank(self, epsilon=0.00001):
+        for hyper_params, full_df in self.dict_all_options_stochastic.items():
+            sim_func, lambda_param = (s.split('-')[1] for s in hyper_params.split('+'))
+            for pred_score in self.prediction_scores:
+                _score_list = []
+                for topic, _df in full_df[pred_score].groupby('topic'):
+                    # Set all the PR values to be 1/N
+                    _initial_pr = 1 / len(_df.index.unique('dest'))
+                    pr_sr = pd.Series(data=_initial_pr, index=_df.index.unique('dest'))
+                    _pr_dict = {}
+                    diff = 1.0
+                    while diff > epsilon:
+                        for dest_node, incoming_weights_df in _df.groupby('dest'):
+                            _pr_dict[dest_node] = incoming_weights_df.mul(pr_sr, level='src').sum()
+                        _pr_sr = pd.Series(_pr_dict)
+                        _diff_sr = pr_sr.subtract(_pr_sr, level='dest').abs()
+                        diff = _diff_sr.sum()
+                        pr_sr = _pr_sr
+                    _score_list.append(pr_sr)
+                res_df = pd.concat(_score_list)
+                self.writ_results(res_df, sim_func, pred_score.split('_')[1], lambda_param)
+
+    def writ_results(self, res_df: pd.Series, sim_func, pred_score, lambda_param):
+        dir_path = dp.ensure_dir(f'{self.output_dir}/{sim_func}/{self.predictor}/predictions/')
+        file_name = f'predictions-{pred_score}+lambda+{lambda_param}'
+        res_df.to_csv(path=f'{dir_path}/{file_name}', index=True, sep=' ', float_format='%f')
 
 
 def main(args):
     corpus = args.corpus
-    generate = args.generate
-    # predict = args.predict
-    # queries_group = args.group
-    file_to_load = args.load
-    # quantile = args.quantile
+    predictor = args.predictor
 
     # # Debugging
-    # print('------------!!!!!!!---------- Debugging Mode ------------!!!!!!!----------')
-    # testing_feat = QueryFeatureFactory('ROBUST')
-    # norm_features_df = testing_feat.generate_features()
-    # # norm_features_df.reset_index().to_json('query_features_{}_uqv.JSON'.format(corpus))
+    # print('\n\n\n------------!!!!!!!---------- Debugging Mode ------------!!!!!!!----------\n\n\n')
+    # predictor = 'wig'
+    # corpus = 'ROBUST'
 
     cores = mp.cpu_count() - 1
 
-    if generate:
-        testing_feat = QueryFeatureFactory(corpus)
-        norm_features_df = testing_feat.generate_features()
-
-        _path = f'~/QppUqvProj/Results/{corpus}/test/pageRank'
-        _path = dp.ensure_dir(_path)
-        norm_features_df.reset_index().to_json(f'{_path}/PageRank_Features.JSON')
-
-    elif file_to_load:
-        features_df = features_loader(file_to_load, corpus)
-        print(features_df)
+    pr_obj = PageRank(corpus, predictor, load=True)
+    pr_obj.calc_pagerank()
 
 
 if __name__ == '__main__':
