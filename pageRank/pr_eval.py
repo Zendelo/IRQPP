253cfb76b06a0e2044fb0bdb558529203baa152e
Zendelo <olegzendel@campus.technion.ac.il>
2019-08-14 18:38:33 +0300

    wip

--- pageRank/pr_eval.py
+++ pageRank/pr_eval.py
@@ -8,9 +8,17 @@ from Timer.timer import Timer
 import networkx as nx
 import pprint
 from crossval import CrossValidation
+from queries_pre_process import add_topic_to_qdf
 
 PREDICTORS = ['clarity', 'wig', 'nqc', 'smv', 'rsd', 'qf', 'uef/clarity', 'uef/wig', 'uef/nqc', 'uef/smv', 'uef/qf']
 SIMILARITY_MEASURES = ['Jac_coefficient', 'RBO_EXT_100', 'Top_10_Docs_overlap', 'RBO_FUSED_EXT_100']
+
+
+def calc_stats(full_df: pd.DataFrame, ap_df: pd.DataFrame):
+    add_topic_to_qdf(full_df)
+    for col in full_df.columns:
+        pr_sr = full_df.loc[:, col]
+        pr_sr.groupby()
 
 
 def main(corpus, similarity, predictor):
@@ -20,7 +28,9 @@ def main(corpus, similarity, predictor):
         f'~/QppUqvProj/Results/{corpus}/uqvPredictions/referenceLists/pageRank/raw/{similarity}/{predictor}/predictions/')
     ap_obj = dp.ResultsReader(ap_file, 'ap')
     ap_df = ap_obj.data_df
-    cv_obj = CrossValidation(predictions_dir=predictions_dir, ap_file=ap_file, file_to_load=cv_folds)
+    cv_obj = CrossValidation(predictions_dir=predictions_dir, file_to_load=cv_folds)
+    full_results_df = cv_obj.full_set
+    calc_stats(full_results_df, ap_df)
 
 
 if __name__ == '__main__':
