933a7e479a3c51216786909e296468b7779843c7
Oleg <olegzendel@campus.technion.ac.il>
2019-08-19 23:34:06 +0300

    created initial working version of scoring best/worst

--- pageRank/pr_eval.py
+++ pageRank/pr_eval.py
@@ -2,6 +2,7 @@ import argparse
 from glob import glob
 
 import pandas as pd
+import numpy as np
 
 import dataparser as dp
 from Timer.timer import Timer
@@ -15,10 +16,30 @@ SIMILARITY_MEASURES = ['Jac_coefficient', 'RBO_EXT_100', 'Top_10_Docs_overlap', 
 
 
 def calc_stats(full_df: pd.DataFrame, ap_df: pd.DataFrame):
-    add_topic_to_qdf(full_df)
-    for col in full_df.columns:
-        pr_sr = full_df.loc[:, col]
-        pr_sr.groupby()
+    max_ap_vars = ap_df.groupby('topic').max()
+    min_ap_vars = ap_df.groupby('topic').min()
+    best_results = []
+    worst_results = []
+    for col in full_df.set_index(['topic', 'qid']).columns:
+        pr_df = full_df.loc[:, ['topic', 'qid', col]]
+        best_result = {}
+        worst_result = {}
+        for topic, _df in pr_df.groupby('topic'):
+            _max_var_ap = max_ap_vars.loc[topic].qid
+            _min_var_ap = min_ap_vars.loc[topic].qid
+            pr_of_max = _df.loc[_df['qid'] == _max_var_ap, col].values[0]
+            pr_of_min = _df.loc[_df['qid'] == _min_var_ap, col].values[0]
+            best_var_score = np.count_nonzero(_df[col] < pr_of_max) / len(_df)
+            worst_var_score = np.count_nonzero(_df[col] > pr_of_min) / len(_df)
+            best_result[topic] = {col: best_var_score}
+            worst_result[topic] = {col: worst_var_score}
+        best_results.append(pd.DataFrame.from_dict(best_result, orient='index'))
+        worst_results.append(pd.DataFrame.from_dict(worst_result, orient='index'))
+    best_df = pd.concat(best_results, axis=1)
+    worst_df = pd.concat(worst_results, axis=1)
+    best_df.to_pickle('best_results.pkl')
+    worst_df.to_pickle('worst_results.pkl')
+    return best_df, worst_df
 
 
 def main(corpus, similarity, predictor):
@@ -27,14 +48,21 @@ def main(corpus, similarity, predictor):
     predictions_dir = dp.ensure_dir(
         f'~/QppUqvProj/Results/{corpus}/uqvPredictions/referenceLists/pageRank/raw/{similarity}/{predictor}/predictions/')
     ap_obj = dp.ResultsReader(ap_file, 'ap')
-    ap_df = ap_obj.data_df
+    ap_df = add_topic_to_qdf(ap_obj.data_df)
     cv_obj = CrossValidation(predictions_dir=predictions_dir, file_to_load=cv_folds)
-    full_results_df = cv_obj.full_set
-    calc_stats(full_results_df, ap_df)
+    full_results_df = add_topic_to_qdf(cv_obj.full_set)
+    try:
+        best_file = dp.ensure_file('best_results.pkl')
+        best_df = pd.read_pickle(best_file)
+    except AssertionError:
+        best_df, worst_df = calc_stats(full_results_df, ap_df)
+    print(best_df)
 
 
 if __name__ == '__main__':
     corpus = 'ROBUST'
     similarity = 'Jac_coefficient'
     predictor = 'wig'
+    timer = Timer('Total time')
     main(corpus, similarity, predictor)
+    timer.stop()
