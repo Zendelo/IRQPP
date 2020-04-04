13a36fba81e29748bbe0289477e642f6e37b9393
Zendelo <s3806763@student.rmit.edu.au>
2020-04-05 01:46:35 +1100

    added new metric

--- pageRank/pr_eval.py
+++ pageRank/pr_eval.py
@@ -1,13 +1,17 @@
-import argparse
-from glob import glob
-
-import pandas as pd
 import numpy as np
+import pandas as pd
+from Timer import Timer, timer
 
-import dataparser as dp
-from Timer.timer import Timer
-import networkx as nx
-import pprint
+try:
+    import dataparser as dp
+except ModuleNotFoundError:
+    import sys
+    from pathlib import Path
+
+    script_dir = sys.path[0]
+    # Adding the parent directory to the path
+    sys.path.append(str(Path(script_dir).parent))
+    import dataparser as dp
 from crossval import CrossValidation
 from queries_pre_process import add_topic_to_qdf
 
@@ -15,75 +19,117 @@ PREDICTORS = ['clarity', 'wig', 'nqc', 'smv', 'rsd', 'qf', 'uef/clarity', 'uef/w
 SIMILARITY_MEASURES = ['Jac_coefficient', 'RBO_EXT_100', 'Top_10_Docs_overlap', 'RBO_FUSED_EXT_100']
 
 
-def calc_stats(full_df: pd.DataFrame, ap_df: pd.DataFrame):
-    max_ap_vars = ap_df.loc[ap_df.groupby('topic')['ap'].transform(max) == ap_df['ap']].set_index('topic')
-    min_ap_vars = ap_df.loc[ap_df.groupby('topic')['ap'].transform(min) == ap_df['ap']].set_index('topic')
-    best_results = []
-    worst_results = []
+def calc_best_worst(full_df: pd.DataFrame, ap_df: pd.DataFrame, metric_direction):
+    bw_param = max if metric_direction == 'best' else min
+    _ap_vars = ap_df.loc[ap_df.groupby('topic')['ap'].transform(bw_param) == ap_df['ap']].set_index('topic')
+    _results = []
     for col in full_df.set_index(['topic', 'qid']).columns:
         pr_df = full_df.loc[:, ['topic', 'qid', col]]
-        best_result = {}
-        worst_result = {}
+        _result = {}
         for topic, _df in pr_df.groupby('topic'):
-            _max_var_ap = max_ap_vars.loc[topic].qid
-            _min_var_ap = min_ap_vars.loc[topic].qid
-            if type(_max_var_ap) is str:
-                pr_of_max = _df.loc[_df['qid'] == _max_var_ap, col].values[0]
+            _var_ap = _ap_vars.loc[topic].qid
+            if type(_var_ap) is str:
+                _pr_val = _df.loc[_df['qid'] == _var_ap, col].values[0]
             else:
-                pr_of_max = np.mean(_df.loc[_df['qid'].isin(_max_var_ap), col].values)
-            if type(_min_var_ap) is str:
-                pr_of_min = _df.loc[_df['qid'] == _min_var_ap, col].values[0]
+                _pr_val = np.mean(_df.loc[_df['qid'].isin(_var_ap), col].values)
+            if metric_direction == 'best':
+                _var_score = np.count_nonzero(_df[col] < _pr_val) / len(_df)
             else:
-                pr_of_min = np.mean(_df.loc[_df['qid'].isin(_min_var_ap), col].values)
-            best_var_score = np.count_nonzero(_df[col] < pr_of_max) / len(_df)
-            worst_var_score = np.count_nonzero(_df[col] > pr_of_min) / len(_df)
-            best_result[topic] = {col: best_var_score}
-            worst_result[topic] = {col: worst_var_score}
-        best_results.append(pd.DataFrame.from_dict(best_result, orient='index'))
-        worst_results.append(pd.DataFrame.from_dict(worst_result, orient='index'))
-    best_df = pd.concat(best_results, axis=1)
-    worst_df = pd.concat(worst_results, axis=1)
-    return best_df, worst_df
+                _var_score = np.count_nonzero(_df[col] > _pr_val) / len(_df)
+            _result[topic] = {col: _var_score}
+        _results.append(pd.DataFrame.from_dict(_result, orient='index'))
+    df = pd.concat(_results, axis=1)
+    return df
 
 
-def calc_scores(corpus, similarity, predictor):
-    cv_folds = dp.ensure_file(f'~/QppUqvProj/Results/{corpus}/test/2_folds_30_repetitions.json')
-    ap_file = dp.ensure_file(f'~/QppUqvProj/Results/{corpus}/test/raw/QLmap1000')
-    pkl_dir = dp.ensure_dir(f'~/QppUqvProj/Results/{corpus}/test/pageRank/pkl_files/{predictor}')
+def init_eval(corpus, similarity, predictor):
+    res_dir, data_dir = dp.set_environment_paths()
+    cv_folds = dp.ensure_file(f'{res_dir}/{corpus}/test/2_folds_30_repetitions.json')
+    ap_file = dp.ensure_file(f'{res_dir}/{corpus}/test/raw/QLmap1000')
+    pkl_dir = dp.ensure_dir(f'{res_dir}/{corpus}/test/pageRank/pkl_files/{predictor}')
     predictions_dir = dp.ensure_dir(
-        f'~/QppUqvProj/Results/{corpus}/uqvPredictions/referenceLists/pageRank/raw/{similarity}/{predictor}/predictions')
-    # results_dir = dp.ensure_dir(
-    #     f'QppUqvProj/Results/{corpus}/uqvPredictions/referenceLists/pageRank/scores/{similarity}/{predictor}/predictions')
+        f'{res_dir}/{corpus}/uqvPredictions/referenceLists/pageRank/raw/{similarity}/{predictor}/predictions')
     ap_obj = dp.ResultsReader(ap_file, 'ap')
     ap_df = add_topic_to_qdf(ap_obj.data_df)
     cv_obj = CrossValidation(predictions_dir=predictions_dir, file_to_load=cv_folds)
     full_results_df = add_topic_to_qdf(cv_obj.full_set)
-    best_file = f'{pkl_dir}/{similarity}_best_results.pkl'
-    worst_file = f'{pkl_dir}/{similarity}_worst_results.pkl'
-    # try:
-    #     best_df = pd.read_pickle(dp.ensure_file(best_file))
-    #     worst_df = pd.read_pickle(dp.ensure_file(worst_file))
-    # except AssertionError:
-    #     best_df, worst_df = calc_stats(full_results_df, ap_df)
-    #     best_df.to_pickle(best_file)
-    #     worst_df.to_pickle(worst_file)
-    best_df, worst_df = calc_stats(full_results_df, ap_df)
-    best_df.to_pickle(best_file)
-    worst_df.to_pickle(worst_file)
-    cv_obj.full_set = best_df
-    best_score = cv_obj.calc_test_results()
-    cv_obj.__delattr__('corrs_df')
-    cv_obj.full_set = worst_df
-    worst_score = cv_obj.calc_test_results()
-    return best_score, worst_score
+    return pkl_dir, ap_obj, ap_df, full_results_df, cv_obj
+
+
+@timer
+def best_worst_metric(corpus, similarity, predictor, metric, load=False):
+    assert metric == 'best' or metric == 'worst', f'The function expects a known metric. {metric} was passed'
+    pkl_dir, ap_obj, ap_df, full_results_df, cv_obj = init_eval(corpus, similarity, predictor)
+    _file = f'{pkl_dir}/{similarity}_{metric}_results.pkl'
+    if load:
+        _df = load_exec(_file, calc_best_worst, (full_results_df, ap_df, metric))
+    else:
+        _df = calc_best_worst(full_results_df, ap_df, metric)
+        _df.to_pickle(_file)
+    return calc_s(cv_obj, _df)
+
+
+def calc_s(cv_obj: CrossValidation, full_scores_df: pd.DataFrame):
+    if hasattr(cv_obj, 'corrs_df'):
+        cv_obj.__delattr__('corrs_df')
+    cv_obj.full_set = full_scores_df
+    score = cv_obj.calc_test_results()
+    return score
+
+
+def load_exec(file_to_load, function_to_exec, args):
+    try:
+        _df = pd.read_pickle(dp.ensure_file(file_to_load))
+    except AssertionError:
+        _df = function_to_exec(*args)
+        _df.to_pickle(file_to_load)
+    return _df
+
+
+def func(corpus, similarity, predictor, minmax):
+    pkl_dir, ap_obj, raw_ap_df, full_pr_df, cv_obj = init_eval(corpus, similarity, predictor)
+    _list = []
+    for col in full_pr_df.set_index(['topic', 'qid']).columns:
+        grpby = full_pr_df.loc[:, ['topic', 'qid', col]].set_index('qid').groupby('topic')[col]
+        _qids = grpby.idxmax() if minmax == 'max' else grpby.idxmin()
+        _df = raw_ap_df.loc[raw_ap_df.qid.isin(_qids)].set_index('topic')['ap']
+        _list.append(_df.rename(col))
+    full_ap_df = pd.concat(_list, axis=1)
+    return calc_s(cv_obj, full_ap_df)
 
 
 if __name__ == '__main__':
     # Debugging
-    print('\n\n\n------------!!!!!!!---------- Debugging Mode ------------!!!!!!!----------\n\n\n')
-    predictor = input('What predictor should be used for debugging?\n')
+    # print('\n\n\n------------!!!!!!!---------- Debugging Mode ------------!!!!!!!----------\n\n\n')
+    # predictor = input('What predictor should be used for debugging?\n')
     corpus = 'ROBUST'
-    similarity = 'Jac_coefficient'
+    # similarity = 'Jac_coefficient'
     timer = Timer('Total time')
-    calc_scores(corpus, similarity, predictor)
+    res_dir, data_dir = dp.set_environment_paths()
+    raw_ap_df = dp.add_topic_to_qdf(
+        dp.ResultsReader(dp.ensure_file(f'{res_dir}/{corpus}/test/raw/QLmap1000'), 'ap').data_df)
+    grp_obj = raw_ap_df.groupby('topic')['ap']
+    avg_ap_df = grp_obj.mean()
+    max_ap_df = grp_obj.max()
+    min_ap_df = grp_obj.min()
+    print(f'Average real MAP: {avg_ap_df.mean():.4f}')
+    print(f'Maximum real MAP: {max_ap_df.mean():.4f}')
+    print(f'Min real MAP: {min_ap_df.mean():.4f}')
+    result = {}
+    for similarity in SIMILARITY_MEASURES:
+        print(f'Similarity {similarity}:')
+        for predictor in PREDICTORS:
+            max_ap_score = func(corpus, similarity, predictor, 'max')
+            min_ap_score = func(corpus, similarity, predictor, 'min')
+            print(f'The MAP score using {predictor} for max PR queries: {max_ap_score}\n'
+                  f'The MAP score using {predictor} for min PR queries: {min_ap_score}')
+            best_score = best_worst_metric(corpus, similarity, predictor, metric='best', load=True)
+            worst_score = best_worst_metric(corpus, similarity, predictor, metric='worst', load=True)
+            print(f'Predicting using {predictor} best var: {best_score}\n'
+                  f'Predicting using {predictor} worst var {worst_score}')
+            result[similarity, predictor] = {'Max MAP': max_ap_score, 'Min MAP': min_ap_score, 'Best Score': best_score,
+                                             'Worst Score': worst_score}
+    df = pd.DataFrame.from_dict(result, orient='index')
+    df.to_pickle(f'{corpus}_PageRank_results_table.pkl')
+    print(df.to_latex(header=True, multirow=True, multicolumn=False, index=True, escape=False, index_names=True))
     timer.stop()
